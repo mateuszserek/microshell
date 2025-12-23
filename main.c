9618702c4e4ed1e285f0907fd6edbfce52d84dc2
Mateusz Serek <mateuszserek90@gmail.com>
2025-12-23 20:17:49 +0100

    some variables moved to global, history of commands

--- main.c
+++ main.c
@@ -3,23 +3,48 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <errno.h>
 
 #define DIR_SIZE 4096
-#define INPUT_SIZE 64
+#define INPUT_SIZE 256
 #define MAX_ARGS 16
+#define HISTORY_FILE_NAME "history.txt"
 
-void set_working_directory(char *dir) {
-    if(getcwd(dir, DIR_SIZE) == NULL) {
+char *history_file_directory;
+char working_directory[DIR_SIZE];
+char *home_directory;
+char *user_name;
+
+void set_working_directory() {
+    if(getcwd(working_directory, DIR_SIZE) == NULL) {
         perror("Error while using getcwd()");
     }
 }
 
-void cd_command(char *path, char *working_dir, char *home_dir) {
+void set_history_directory() {
+    history_file_directory = malloc(strlen(working_directory) + strlen(HISTORY_FILE_NAME) + 2);
+    strcat(history_file_directory, working_directory);
+    strcat(history_file_directory, "/");
+    strcat(history_file_directory, HISTORY_FILE_NAME);
+    creat(history_file_directory, 0644);
+}
+
+void save_input_into_history(char *input) {
+    if (strcmp(input, "\n") == 0) {
+        return;
+    }
+    int history_file_directory_fd = open(history_file_directory, O_WRONLY | O_APPEND);
+    write(history_file_directory_fd, input, strlen(input));
+    close(history_file_directory_fd);
+}
+
+void cd_command(char *path) {
     int cd_status;
     if (path == NULL) {
-        cd_status = chdir(home_dir);
+        cd_status = chdir(home_directory);
     } else {
         cd_status = chdir(path);
     }
@@ -27,7 +52,7 @@ void cd_command(char *path, char *working_dir, char *home_dir) {
     if(cd_status != 0) {
         perror("Couldn't change directory");
     }
-    set_working_directory(working_dir);
+    set_working_directory();
 }
 
 void parse_input(char *input, char *args[]) {
@@ -40,8 +65,8 @@ void parse_input(char *input, char *args[]) {
     while (ptr != NULL && i < MAX_ARGS - 1) {
         args[i] = malloc(strlen(ptr) + 1);
         strcpy(args[i], ptr);
-        i++;
         ptr = strtok(NULL, " \t\n");
+        i++;
     }
     args[i] = NULL;
 }
@@ -53,8 +78,9 @@ void remove_nl(char *str) {
     }
 }
 
-void handle_command(char *input, char *working_dir, char *home_dir) {
+void handle_command(char *input) {
     char *function_args[MAX_ARGS];
+    save_input_into_history(input);
     remove_nl(input);
     parse_input(input, function_args);
     char *command_name = function_args[0];
@@ -68,14 +94,16 @@ void handle_command(char *input, char *working_dir, char *home_dir) {
     }
 
     if (strcmp(command_name, "cd") == 0) {
-        cd_command(function_args[1], working_dir, home_dir);
+        cd_command(function_args[1]);
         return;
     }
 
     pid_t ps = fork();
     if (ps == 0) {
-        execvp(command_name, function_args);
-        perror(command_name);
+        if (execvp(command_name, function_args) == -1) {
+            perror(command_name);
+            exit(errno);
+        }
         exit(0);
     } else {
         wait(NULL);
@@ -83,15 +111,16 @@ void handle_command(char *input, char *working_dir, char *home_dir) {
 }
 
 int main(int argc, char *argv[]) {
-    char working_directory[DIR_SIZE];
     char input[INPUT_SIZE];
-    char *home_dir = getenv("HOME");
-    set_working_directory(working_directory);
+    home_directory = getenv("HOME");
+    user_name = getenv("USER");
+    set_working_directory();
+    set_history_directory();
 
     while(1) {
-        printf("%s: ", working_directory);
+        printf("%s:%s$ ", user_name, working_directory);
         fgets(input, sizeof(input), stdin);
-        handle_command(input, working_directory, home_dir);
+        handle_command(input);
     }
     return 0;
 }
